3602ac30a0d1bf4bb4543bcda4d7cfec3de10fb6
Nick Dolan <nicholas.dolan1@gmail.com>
2022-11-30 23:29:41 -0600

    Day 1 implementation in Rust

---
+++ src/day1.rs
@@ -0,0 +1,21 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+type Input = Vec<Vec<u64>>;
+
+#[aoc_generator(day1)]
+pub fn day1_generator(input: &str) -> Input {
+    input.split("\n\n").map(|s| s.lines().map(|l| l.parse::<u64>().unwrap()).collect()).collect()
+}
+
+#[aoc(day1, part1)]
+pub fn solve_part1(input: &Input) -> u64 {
+    input.iter().map(|cal| cal.iter().sum::<u64>()).max().unwrap()
+}
+
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &Input) -> u64 {
+    let mut calorie_list = input.iter().map(|cal| cal.iter().sum::<u64>()).collect::<Vec<_>>();
+    calorie_list.sort_unstable();
+
+    calorie_list.iter().rev().take(3).sum()
+}
