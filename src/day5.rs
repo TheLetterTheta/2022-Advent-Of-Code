7011d4e68a77c339150e5a21b651997aaceb3775
Nick Dolan <nicholas.dolan1@gmail.com>
2022-12-05 07:00:00 -0600

    Replace lazy_static with oncecell

--- src/day5.rs
+++ src/day5.rs
@@ -1,8 +1,14 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-use lazy_static::lazy_static;
 use regex::Regex;
 
 type Input = (Vec<Vec<char>>, Vec<(u32, usize, usize)>);
+
+macro_rules! regex {
+    ($re:literal $(,)?) => {{
+        static RE: once_cell::sync::OnceCell<regex::Regex> = once_cell::sync::OnceCell::new();
+        RE.get_or_init(|| regex::Regex::new($re).unwrap())
+    }};
+}
 
 #[aoc_generator(day5)]
 pub fn day5_generator(input: &str) -> Input {
@@ -18,9 +24,7 @@ pub fn day5_generator(input: &str) -> Input {
         "TBHFPDCM".chars().rev().collect(),
     ];
 
-    lazy_static! {
-        static ref RE: Regex = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
-    }
+    let expr: &Regex = regex!(r"move (\d+) from (\d+) to (\d+)");
 
     (
         stacks,
@@ -28,7 +32,7 @@ pub fn day5_generator(input: &str) -> Input {
             .lines()
             .skip(10)
             .map(|line| {
-                let captures = RE.captures(line).unwrap();
+                let captures = expr.captures(line).unwrap();
                 (
                     captures.get(1).unwrap().as_str().parse::<u32>().unwrap(),
                     captures.get(2).unwrap().as_str().parse::<usize>().unwrap() - 1,
@@ -43,29 +47,29 @@ pub fn day5_generator(input: &str) -> Input {
 pub fn solve_part1(input: &Input) -> String {
     let (mut state, instructions) = input.clone();
 
-    for (num, from, to) in instructions.iter() {
-        for _ in 0..*num {
-            let popped = state[*from].pop().unwrap();
-            state[*to].push(popped);
+    for (num, from, to) in instructions {
+        for _ in 0..num {
+            let popped = state[from].pop().unwrap();
+            state[to].push(popped);
         }
     }
-    state.iter().map(|m| m.iter().rev().next().unwrap()).collect::<String>()
+    state.iter().map(|m| m.last().unwrap()).collect::<String>()
 }
 
 #[aoc(day5, part2)]
 pub fn solve_part2(input: &Input) -> String {
     let (mut state, instructions) = input.clone();
 
-    for (num, from, to) in instructions.iter() {
+    for (num, from, to) in instructions {
         let mut intermediate = vec![];
-        for _ in 0..*num {
-            let popped = state[*from].pop().unwrap();
+        for _ in 0..num {
+            let popped = state[from].pop().unwrap();
             intermediate.push(popped);
         }
 
         while let Some(i) = intermediate.pop() {
-            state[*to].push(i);
+            state[to].push(i);
         }
     }
-    state.iter().map(|m| m.iter().rev().next().unwrap()).collect::<String>()
+    state.iter().map(|m| m.last().unwrap()).collect::<String>()
 }
